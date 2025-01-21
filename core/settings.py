9b908b1e3b4b11874763f61255557ec8590cb95a
ArielMota <arieloliveira08@gmail.com>
2025-01-21 15:50:36 -0300

    Atualização DATABASES

--- core/settings.py
+++ core/settings.py
@@ -85,27 +85,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#'default': {
-#'ENGINE': 'django.db.backends.postgresql',
-#'NAME': os.getenv('DB_NAME'),
-#'USER': os.getenv('DB_USER'),
-#'PASSWORD': os.getenv('DB_PASSWORD'),
-#'HOST': os.getenv('DB_HOST'),
-#'PORT': os.getenv('DB_PORT'),
-#}
-#}
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sgevendb',
-        'USER': 'sgevenuser',
-        'PASSWORD': 'kLLsBEzoQOSIYsO2xUF6cWpuDgNMCWlV',
-        'HOST': 'dpg-cu7c02ggph6c73b9tv2g-a',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 
 
